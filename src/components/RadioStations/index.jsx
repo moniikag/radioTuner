d0e9dc6afc35cc83a2fecbffe4d6e6ac80be7c6e
Monika Glier <monikaglier@gmail.com>
2017-12-18 16:17:11 +0100

    Toggle button & form visibility

--- src/components/RadioStations/index.jsx
+++ src/components/RadioStations/index.jsx
@@ -12,26 +12,45 @@ class RadioStations extends Component {
         { name: 'Best Hits Ever', frequency: '97.5' },
         { name: 'Classic EU', frequency: '109.3' },
       ],
+      addFormOpen: false,
     }
+    this.toggleForm = this.toggleForm.bind(this)
+  }
+
+  toggleForm() {
+    return this.setState(
+      (prevState) => ({ addFormOpen: !prevState.addFormOpen })
+    )
   }
 
   render() {
-    const { stations } = this.state
+    const { addFormOpen, stations } = this.state
 
     return (
       <ul className="radio-stations-list">
         {stations.map(station =>
           <RadioStation key={station.name} station={station} />
         )}
-        <li>
-          <button className="add"> + </button>
-        </li>
-        <li>
+        {!addFormOpen && <li>
+          <button
+            className="add"
+            onClick={this.toggleForm}
+          >
+            +
+          </button>
+        </li>}
+        {addFormOpen && <li id="form">
+          <button
+            id="close"
+            onClick={this.toggleForm}
+          >
+            x
+          </button>
           <form>
             <input id="name" type="text" placeholder="name" />
             <input type="text" placeholder="frequency" />
           </form>
-        </li>
+        </li>}
       </ul>
     )
   }
