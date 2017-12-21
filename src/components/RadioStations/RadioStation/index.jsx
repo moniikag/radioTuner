fdeac958a076f45c74053a371b45ed2332987819
Monika Glier <monikaglier@gmail.com>
2017-12-21 12:53:20 +0100

    Link to Single Station from main page

--- src/components/RadioStations/RadioStation/index.jsx
+++ src/components/RadioStations/RadioStation/index.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom'
 
 const RadioStation = ({ station }) => {
 
   return (
     <li>
-      <header>{station.name}</header>
+      <Link
+        className="station-link"
+        to={`/station/${station.name.split(' ').join('_')}`}
+      >
+        <header>{station.name}</header>
+      </Link>
       Frequency:<span className="detail">{station.frequency} FM</span>
     </li>
   )
