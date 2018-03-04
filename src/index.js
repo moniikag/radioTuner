dd07417f603ce4da230be23716e92e92269c5d9d
Monika Glier <monikaglier@gmail.com>
2018-03-04 17:17:22 +0100

    Enable redux devtools

--- src/index.js
+++ src/index.js
@@ -7,7 +7,10 @@ import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 
 import radioStations from './reducers'
-let store = createStore(radioStations)
+let store = createStore(
+  radioStations,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
 
 ReactDOM.render(
   <Provider store={store}>
